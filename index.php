73ca5a584871d6cca4fc7b70460d5fe0e1c955ed
El-Tawfik <kifwat17@gmail.com>
2022-05-31 14:06:29 +0100

    i delete var_dump on 7 line

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
   $priority=filter_input(INPUT_POST,"priority",FILTER_VALIDATE_INT);
   $type=filter_input(INPUT_POST,"type",FILTER_VALIDATE_INT);
   $terms=filter_input(INPUT_POST,"terms",FILTER_VALIDATE_BOOL);
-  var_dump($name,$message,$priority,$type,$terms);
   if(!$terms){
       die("terms must be accepted");
 
